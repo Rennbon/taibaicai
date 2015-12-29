8e9b6cebcaca484bc9424a27084eeb00e7342b9d
Rennbon <343688972@qq.com>
2015-12-29 13:30:51 +0800

    123

--- Test/Program.cs
+++ Test/Program.cs
@@ -19,23 +19,36 @@ namespace Test
               new
               {
                   email = "343688972@qq.com",
-                  password="111111",
+                  password = "111111",
                   token = "59ea927e-3a4a-4387-97c4-158071e31fce",
-                  userID=20,
-                  id=1 
+                  userID = 20,
+                  id = 1
               }
               );
 
-            HttpClient clinet = new HttpClient();
-            MediaTypeFormatter jsonFormatter = new JsonMediaTypeFormatter();
+            //HttpClient clinet = new HttpClient();
+            //MediaTypeFormatter jsonFormatter = new JsonMediaTypeFormatter();
 
-            HttpContent content = new ObjectContent<string>(jsonContent, jsonFormatter);
-            var rsp = clinet.PostAsync(new Uri("http://172.17.10.196:9999/API/Event/PushMessage"), content);
-            var stream = rsp.Result.Content;
-            string jsonre = string.Empty;
-            using (var read = new StreamReader(stream.ReadAsStreamAsync().Result))
+            //HttpContent content = new ObjectContent<string>(jsonContent, jsonFormatter);
+
+            //var rsp = clinet.PostAsync(new Uri("http://172.17.10.196:9999/API/Event/PushMessage"), content);
+            //var stream = rsp.Result.Content;
+            //string jsonre = string.Empty;
+            //using (var read = new StreamReader(stream.ReadAsStreamAsync().Result))
+            //{
+            //    jsonre = read.ReadToEnd();
+            //}
+            string json = string.Empty;
+            using (var client = new HttpClient())
             {
-                jsonre = read.ReadToEnd();
+                var res = client.PostAsync(new Uri("http://172.17.10.196:9999/API/User/Login"), new { email = "343688972@qq.com", password = "111111" }, new JsonMediaTypeFormatter());
+                var stream = res.Result.Content;
+               
+                using (var read = new StreamReader(stream.ReadAsStreamAsync().Result))
+                {
+                    json = read.ReadToEnd();
+                }
+
             }
 
             //var stream = clinet.GetAsync(new Uri("http://172.17.10.196:9999/API/User/GetUserInfo?userID=1")).Result.Content;
@@ -46,9 +59,9 @@ namespace Test
             //    jsonre = streamRead.ReadToEnd();
             //    var a = JsonConvert.DeserializeObject(jsonre);
             //}
-            
-            
-            Console.WriteLine(jsonre);
+
+
+            Console.WriteLine(json);
 
             //new EventBLL().BirthdayRemind();
             Console.ReadKey();
